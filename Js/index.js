322e6140fd3762872207128c73fbe1a1b6cfcc86
sahanux <sahanakalanka818@gmail.com>
2022-10-02 10:29:09 +0530

    First day js practicals updated

---
+++ Js/index.js
@@ -0,0 +1,8 @@
+var university = "AMDT";
+console.log(university);
+// declare
+var college;
+college = "AMDT"; 
+console.log(college);
+
+
---
+++ Js/practical.js
@@ -0,0 +1,12 @@
+function fullName() {
+    var firstName = "Sahan";
+var lastName = "Akalanka";
+var isCheck = true;
+if (isCheck) {
+    console.log(firstName + " " + lastName);
+}
+else
+console.log("Error!");
+}
+
+fullName();
